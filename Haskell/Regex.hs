11d08c3f6dd80587d057e0e10bda124ed35c79f9
Meowcolm024 <luoyicong1026@live.com>
2021-09-12 14:20:04 +0800

    simple regex

---
+++ Haskell/Regex.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE RankNTypes #-}
+module Haskell.Regex where
+
+data Result = A | O | P deriving Show
+
+data RegEx a = Epsilon
+             | Atom a
+             | Or (RegEx a) (RegEx a)
+             | Then (RegEx a) (RegEx a)
+             | Star (RegEx a)
+             deriving Show
+
+type RegChr = RegEx Char
+type RegInt = RegEx Int
+type RegMatch a = forall a . Eq a => RegEx a -> [a] -> (Result, [a])
+
+(+++) :: (Result, [a]) -> (Result, [a]) -> (Result, [a])
+rx@(A, _) +++ _         = rx
+(   P, _) +++ ry@(A, _) = ry
+_         +++ ry        = ry
+
+matchRegEx :: RegMatch a
+matchRegEx Epsilon []                 = (A, [])
+matchRegEx (Atom x) (e : es) | x == e = (A, es)
+matchRegEx (Or rx ry) es              = matchRegEx rx es +++ matchRegEx ry es
+matchRegEx (Then rx ry) es
+    | (A, es') <- matchRegEx rx es = matchRegEx ry es'
+    | (P, es') <- matchRegEx rx es = matchRegEx ry es'
+matchRegEx (Star _) [] = (A, [])
+matchRegEx s@(Star r) es | (A, []) <- matchRegEx r es  = (A, [])
+                         | (A, es') <- matchRegEx r es = matchRegEx s es'
+                         | (O, es') <- matchRegEx r es = (P, es')
+matchRegEx _ es = (O, es)
+
+match :: RegMatch a
+match r e | (A, x@(_ : _)) <- result = (O, x)
+          | (P, x) <- result         = (O, x)
+          | otherwise                = result
+    where result = matchRegEx r e
+
+zeros :: RegInt
+zeros = Then (Atom 0) (Star (Atom 0))
+
+aabbs :: RegChr
+aabbs = Then (Star (Atom 'a')) (Atom 'b')
+
+-- >>> match (Star (Then (Atom 0) (Atom 1))) [0,1,0,1,0,1]
+-- (A,[])
