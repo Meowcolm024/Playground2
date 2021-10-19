212a4cdcf8c9ab5e3ea22835c382644a49b3ddee
Meowcolm024 <luoyicong1026@live.com>
2021-10-19 10:47:44 +0800

    omg!

---
+++ Cpp5/dang.cpp
@@ -0,0 +1,54 @@
+#include <cstring>
+#include <cstdio>
+#include <cinttypes>
+
+constexpr auto n = 10;
+constexpr int64_t fac(int64_t x)
+{
+    return x == 0 ? 1 : x * fac(x - 1);
+}
+constexpr auto facn = fac(n);
+
+struct A
+{
+    int a;
+};
+
+struct B
+{
+    char a;
+    char b;
+    char c;
+    char d;
+};
+
+class C
+{
+private:
+    int a;
+
+public:
+    int b;
+
+    void show() {printf("C: %d %d\n", a, b);}
+};
+
+int main()
+{
+    auto a = A{4};
+    auto b = B{'a', 'b', 'c', 'd'};
+
+    memcpy(&a, &b, 4);
+
+    printf("A: %d\nB: %c%c%c%c\n", a.a, b.a, b.b, b.c, b.d);
+
+    auto c = C();
+    c.b = 10;
+    *(&(c.b)-1) = 20;
+    c.show();
+
+    auto x = &C::show;
+    printf("%p\n", (void*)x);
+
+    return 0;
+}
---
+++ Cpp5/desseq.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Droppable
+{
+    string name;
+    Droppable(string n = "anonymous") : name(n) {}
+    Droppable(const Droppable& d) {
+        name = d.name;
+        cout << "> Constructing via copying " << name << endl;
+    }
+    Droppable(Droppable &&d)
+    {
+        name = move(d.name);
+        cout << "> Constructing via moving " << name << endl;
+    }
+    ~Droppable() { cout << "> Dropping " << name << endl; }
+    Droppable& operator=(const Droppable& d) {
+        name = d.name;
+        cout << "> Copying " << name << endl;
+        return *this;
+    }
+    Droppable& operator=(Droppable&& d) {
+        name = move(d.name);
+        cout << "> Moving " << name << endl;
+        return *this;
+    }
+};
+
+int main()
+{
+    Droppable ds[3];
+
+    {
+        auto d = Droppable("Bonjour");
+        Droppable e("Hello");
+        auto f = move(e);
+        auto g = d;
+        // ? DESTRUCTOR USE AFTER MOVE?
+    }
+
+    for (auto i = 0; i < 3; i++)
+        ds[i].name = to_string(i);
+
+    return 0;
+}
