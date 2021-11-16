04e6fbf0cb73586ef565fe2977d67a99dbfa61b9
Meowcolm024 <luoyicong1026@live.com>
2021-11-16 10:41:28 +0800

    state

--- Haskell3/Poly.hs
+++ Haskell3/Poly.hs
@@ -1,2 +1,27 @@
 module Poly where
 
+import           Control.Monad
+import           Control.Monad.ST
+import           Control.Monad.Trans.State
+import           Data.Functor
+import           Data.STRef
+
+countTrue :: [Bool] -> Int
+countTrue arr = runST $ do
+    i <- newSTRef 0
+    forM_ arr $ \n -> do
+        when n $ modifySTRef i (+ 1)
+    readSTRef i
+
+data Tree = Leaf Int | Node Tree Tree deriving Show
+
+countPos tree = do
+    n <- get
+    case tree of
+        Leaf i   -> when (i > 0) $ put (n + 1)
+        Node l r -> countPos l *> countPos r
+
+test = Node (Node (Leaf 1) (Node (Leaf (-1)) (Leaf 3)))
+            (Node (Node (Leaf (-4)) (Leaf (-2))) (Leaf 4))
+
+resutl = execState (countPos test) 0
--- Haskell3/Ty.hs
+++ Haskell3/Ty.hs
@@ -7,3 +7,11 @@ module Ty where
 a = id @Int 3
 
 b = id @(Int -> Int) (id @Int)
+
+newtype MInt = MInt Int deriving (Show, Eq)
+
+f :: Semigroup a => a -> a -> a
+f x y = x <> y <> y <> x
+
+instance Semigroup MInt where
+  MInt i <> MInt j = MInt $ i + j
