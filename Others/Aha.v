f08164151a596f8069908abed9994b104377c6fa
Meowcolm024 <luoyicong1026@live.com>
2022-02-14 12:23:55 +0800

    cleanup

--- Others/Aha.v
+++ Others/Aha.v
@@ -1,16 +1,15 @@
 Require Import Init.Datatypes.
 From Coq Require Recdef.
-Require Import Init.Nat.
 From Coq Require Import Lia.
 
 Inductive Term :=
 | t
 | f
 | zero
-| succ      (t : Term)
-| pred      (t : Term)
-| iszero    (t : Term)
-| cond      (p : Term) (t1 : Term) (t2 : Term).
+| succ    (t : Term)
+| pred    (t : Term)
+| iszero  (t : Term)
+| cond    (p : Term) (t1 : Term) (t2 : Term).
 
 Fixpoint isnv (tm: Term) : bool :=
     match tm with
@@ -34,21 +33,20 @@ Fixpoint reduce (tm : Term) : option Term :=
     | _                 => None
     end.
 
-(* size *)
-Fixpoint depth (tm : Term) : nat :=
+Fixpoint size (tm : Term) : nat :=
     match tm with
     | zero | t | f  => 1
-    | succ n        => 1 + depth n
-    | iszero n      => 1 + depth n
-    | pred n        => 1 + depth n
-    | cond p t1 t2  => 1 + (depth p) + (depth t1) + (depth t2)
+    | succ n        => 1 + size n
+    | iszero n      => 1 + size n
+    | pred n        => 1 + size n
+    | cond p t1 t2  => 1 + (size p) + (size t1) + (size t2)
     end.
 
 (* too obvious *)
 Lemma le_S : forall (a b : nat), a < b -> S a < S b. Proof. intros. lia. Qed.
 Lemma le_O : forall (a : nat), 0 < S a. Proof. intros. lia. Qed.
 
-Function eval (tm : Term) {measure depth tm} : Term :=
+Function eval (tm : Term) {measure size tm} : Term :=
     match reduce tm with
     | None      => tm
     | Some tm'  => eval tm'
@@ -63,65 +61,45 @@ Proof.
       + inversion H0.
     - destruct (reduce tm) eqn: T.
       + destruct tm; inversion T.
-        {
-            simpl in teq. destruct (isnv tm).
-            inversion H0. rewrite <- H2. simpl. 
-            lia. inversion H0.
-        }
-        {
-            simpl in H0. inversion H0. simpl.
-            assert (S (depth t0) < S(depth(pred tm))).
-            apply le_S. apply IHtm. reflexivity.
-            simpl in H. apply H.
-        }
-        {
-            simpl in H0. inversion H0. 
-            assert (S (depth t0) < S(depth(iszero tm))).
-            apply le_S. apply IHtm. reflexivity.
-            simpl in H. apply H.
-        }
-        {
-            simpl in H0. inversion H0.
-            assert (S (depth t0) < S(depth(cond tm1 tm2 tm3))).
-            apply le_S. apply IHtm. reflexivity.
-            simpl in H. apply H.
-        } 
-     + destruct tm; inversion H0.
-       { auto. }
-       { 
-            destruct (isnv tm); inversion H1.
-            simpl. lia.
-       }
+        * simpl in teq. destruct (isnv tm).
+          inversion H0. rewrite <- H2. simpl. 
+          lia. inversion H0.
+        * simpl in H0. inversion H0. simpl.
+          assert (S (size t0) < S(size(pred tm))).
+          apply le_S. apply IHtm. reflexivity.
+          simpl in H. apply H.
+        * simpl in H0. inversion H0. 
+          assert (S (size t0) < S(size(iszero tm))).
+          apply le_S. apply IHtm. reflexivity.
+          simpl in H. apply H.
+        * simpl in H0. inversion H0.
+          assert (S (size t0) < S(size(cond tm1 tm2 tm3))).
+          apply le_S. apply IHtm. reflexivity.
+          simpl in H. apply H. 
+      + destruct tm; inversion H0.
+        * auto.
+        * destruct (isnv tm); inversion H1.
+          simpl. lia.
     - destruct (reduce tm) eqn: T.
       + destruct tm; inversion T.
-        {
-            simpl in teq. destruct (isnv tm); inversion H0.
-            simpl. lia.
-        }
-        {
-            simpl in teq. destruct (isnv tm);
-            simpl in H0; inversion H0;
-            assert (S (depth t0) < S(depth (pred tm)));
-            simpl; apply le_S; apply IHtm; reflexivity;
-            apply H.
-        }
-        {
-            simpl in H0. inversion H0.
-            assert (S (depth t0) < S (depth (iszero tm))).
-            apply le_S. apply IHtm. reflexivity. apply H.
-        }
-        {
-            simpl in H0. inversion H0.
-            assert (S (depth t0) < S(depth(cond tm1 tm2 tm3))).
-            apply le_S. apply IHtm. reflexivity.
-            simpl in H. apply H.
-        }
+        * simpl in teq. destruct (isnv tm); inversion H0.
+          simpl. lia.
+        * simpl in teq. destruct (isnv tm);
+          simpl in H0; inversion H0;
+          assert (S (size t0) < S(size (pred tm)));
+          simpl; apply le_S; apply IHtm; reflexivity;
+          apply H.
+        * simpl in H0. inversion H0.
+          assert (S (size t0) < S (size (iszero tm))).
+          apply le_S. apply IHtm. reflexivity. apply H.
+        * simpl in H0. inversion H0.
+          assert (S (size t0) < S(size(cond tm1 tm2 tm3))).
+          apply le_S. apply IHtm. reflexivity.
+          simpl in H. apply H.
       + destruct tm; inversion H0.
-        { simpl. lia. }
-        { 
-            destruct (isnv tm); inversion H0.
-            simpl. lia.
-        }
+        * simpl. lia.
+        * destruct (isnv tm); inversion H0.
+          simpl. lia.
     - destruct (reduce tm1) eqn: T.
       + destruct tm1.
         * simpl. inversion H0. lia.
@@ -129,21 +107,21 @@ Proof.
         * inversion teq.
         * simpl in H0. inversion H0.
           simpl. apply le_S.
-          assert (depth t0 < depth (succ tm1)).
-          apply IHtm1. reflexivity.
-        simpl in H. lia.
-        * simpl in H0. inversion H0.
-          simpl. apply le_S.
-          assert (depth t0 < depth (pred tm1)).
+          assert (size t0 < size (succ tm1)).
           apply IHtm1. reflexivity.
           simpl in H. lia.
         * simpl in H0. inversion H0.
           simpl. apply le_S.
-          assert (depth t0 < depth (iszero tm1)).
+          assert (size t0 < size (pred tm1)).
+          apply IHtm1. reflexivity.
+          simpl in H. lia.
+        * simpl in H0. inversion H0.
+          simpl. apply le_S.
+          assert (size t0 < size (iszero tm1)).
           apply IHtm1. reflexivity.
           simpl in H. lia.
         * simpl in H0. inversion H0. simpl. apply le_S.
-          assert (depth t0 < depth (cond tm1_1 tm1_2 tm1_3)).
+          assert (size t0 < size (cond tm1_1 tm1_2 tm1_3)).
           apply IHtm1. reflexivity. 
           simpl in H. lia.
       + destruct tm1; inversion H0.
